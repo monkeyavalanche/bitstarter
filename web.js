3a963ede7ff4578148cd788279da51e6b1be041a
monkeyavalanche <kahlil2000@gmail.com>
2013-07-23 03:49:21 +0000

    added web.js

--- web.js
+++ web.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
-var outfile = fs.readFileSync('./index.html',function(err,data){
-  if(err) throw err;
-  console.log(data);
-});
-var buffer = new Buffer(256);
-
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer(outfile,));
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
